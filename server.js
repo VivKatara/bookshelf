0ad047ddfeb5bfecd79e543caf639d9b96b458a2
Vivek Katara <vivekkatara@Viveks-MacBook-Pro.local>
2020-06-07 14:29:07 -0400

    Authentication and Authorization workflow for access and refresh tokens works manually. Though, I think it would be best to store the user object in a session, and will be using passport.js local strategy next. Come back to this commit to see how it was done manually.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const authenticateToken = require("./validation/authenticateToken");
+// const authenticateToken = require("./validation/authenticateToken");
 
 // Connect to DB
 mongoose
